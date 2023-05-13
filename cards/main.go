7a59b27e71eac5059bee03c8545917b132d523c3
barrystone <barrystone3183@gmail.com>
2023-05-13 08:32:40 +0800

    add basic variable declarations

---
+++ cards/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var test int
+
+func main() {
+	// var card string = "This is a crad"
+	card := "This is a card"
+
+	card = "This is still a card"
+	test = 66
+
+	fmt.Println(card)
+	fmt.Println(test)
+}
